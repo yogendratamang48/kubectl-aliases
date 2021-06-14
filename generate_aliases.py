4bee263632345fff16a537d1ee06e0e09958f734
yogendratamang48 <yogen48@hotmail.com>
2021-06-14 22:36:51 +0200

    Add Role/Rolebindings ClusterRole/ClusterRoleBindings

--- generate_aliases.py
+++ generate_aliases.py
@@ -56,6 +56,8 @@ def main():
         ('sec', 'secret', ['g', 'd', 'rm'], None),
         ('no', 'nodes', ['g', 'd'], ['sys']),
         ('ns', 'namespaces', ['g', 'd', 'rm'], ['sys']),
+        ('ro', 'rolebindings', ['g', 'd', 'rm'], ['sys']),
+        ('cro', 'clusterrolebindings', ['g', 'd', 'rm'], ['sys']),
         ]
     res_types = [r[0] for r in res]
 
